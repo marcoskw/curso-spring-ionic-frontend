02de8e172259139bef8a0a732f333bde0a3c6ae9
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-12-14 11:16:44 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -27,8 +27,6 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response;
       }, 
-      error => {
-        console.log(error);
-      });
+      error => {});
   }
 }
