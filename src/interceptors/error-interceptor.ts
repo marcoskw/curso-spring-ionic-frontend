6d08282a746836aa590b4962d2bd0294a7eddd68
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-12-23 16:06:59 -0300

    Salvando um novo Cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
-import { AlertController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
     constructor(public storage: StorageService, public alertCtrl: AlertController) {
-
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
         return next.handle(req)
         .catch((error, caught) => {
 
@@ -36,44 +35,70 @@ export class ErrorInterceptor implements HttpInterceptor {
                 this.handle403();
                 break;
 
-                default:
-                this.handleDefaultError(errorObj);
+                case 422:
+                this.handle422(errorObj);
                 break;
+
+                default:
+                this.handleDefaultEror(errorObj);
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
-    handle401() {
-        let alert = this.alertCtrl.create({
-            title: 'Erro 401: Falha de autenticação',
-            message: 'Email ou Senha incorretos',
-            enableBackdropDismiss: false,
-            buttons: [
-                {
-                    text: 'OK'
-                }
-            ]
-        });
-        alert.present();
-    }
     handle403() {
         this.storage.setLocalUser(null);
     }
 
-    handleDefaultError(errorObj) {
+    handle401() {
         let alert = this.alertCtrl.create({
-            title: 'Erro ' +errorObj.status + ': ' + errorObj.error,
-            message: errorObj.message,
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
             enableBackdropDismiss: false,
             buttons: [
                 {
-                    text: 'OK'
+                    text: 'Ok'
                 }
             ]
         });
         alert.present();
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();        
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,15 +1,14 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { API_CONFIG } from "../../config/api.config";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
+import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
 
 @Injectable()
 export class ClienteService {
 
     constructor(public http: HttpClient, public storage: StorageService) {
-
     }
 
     findByEmail(email: string) : Observable<ClienteDTO> {
@@ -19,5 +18,16 @@ export class ClienteService {
     getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
+    }
+
+    insert(obj : ClienteDTO) {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes`, 
+            obj,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
     }
 }
