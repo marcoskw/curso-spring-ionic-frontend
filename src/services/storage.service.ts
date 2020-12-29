b2dc531fbda1c5a09a293c44ef1ee4a6f027446b
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2020-12-29 15:39:04 -0300

    Criando páginade Carrinho de Compras

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
+import { Cart } from "../models/cart";
 import { LocalUser } from "../models/local_user";
 
 @Injectable()
@@ -21,6 +22,25 @@ export class StorageService {
         }
         else {
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+        }
+    }
+
+    getCart() : Cart {
+        let str = localStorage.getItem(STORAGE_KEYS.cart);
+        if (str !== null) {
+            return JSON.parse(str);
+        }
+        else {
+            return null;
+        }
+    }
+
+    setCart(obj : Cart) {
+        if (obj != null) {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
+        }
+        else {
+            localStorage.removeItem(STORAGE_KEYS.cart);
         }
     }
 }
