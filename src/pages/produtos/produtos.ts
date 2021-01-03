b86bd9ab27b5e99388b94949c14c2b66da0dfbfa
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2021-01-03 19:30:40 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -55,5 +59,12 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
