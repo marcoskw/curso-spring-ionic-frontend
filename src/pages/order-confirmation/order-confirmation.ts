e64c2443b5d37e833aa3d618561ffb844b59e983
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2021-01-03 17:13:17 -0300

    Informando o pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -23,6 +23,8 @@ export class OrderConfirmationPage {
 
   endereco: EnderecoDTO;
 
+  codPedido: string;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -60,16 +62,25 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403) {
           this.navCtrl.setRoot('HomePage')
         }
       })
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
