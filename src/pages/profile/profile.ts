3aac7f56b898a0097777100c31ce119fda20983d
Marcos Körbes Werle <marcoskorbeswerle@hotmail.com>
2021-01-04 09:19:49 -0300

    Fazendo o upload da Foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,6 +28,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+   this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -70,5 +74,19 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      })
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
